a4e82956de752f565e8e243de8a5ded734d86eab
BilalKabaKhalil <bilalkabakhalil@gmail.com>
2025-07-23 15:46:18 +0300

    - initial project

---
+++ lib/app/checkout_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
